3d5e22304d022e92c90767dcd82e38fc7df4326f
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-09-21 16:19:57 -0300

    cambios realizados en op.js para productos mas baratos. Ivo Tabarcache

--- src/op.js
+++ src/op.js
@@ -27,7 +27,40 @@ console.log(productos);
 function generarID(){
     return productos.length +1;
 }
+document.getElementById("masBarato").onclick = function(){
+    const productosPorNombre = [];
 
+    // Utiliza map para crear un array de productos con el mismo nombre
+    const productosMismoNombre = productos.map(function (elemento) {
+        return productos.filter(function (producto) {
+            return producto.producto === elemento.producto;
+        });
+    });
+
+    // Itera sobre el array resultante y encuentra el producto más barato de cada grupo
+    productosMismoNombre.forEach(function (grupoProductos) {
+        const productoMasBarato = grupoProductos.reduce(function (min, producto) {
+            if (producto.precio < min.precio) {
+                return producto;
+            } else {
+                return min;
+            }
+        }, grupoProductos[0]);
+
+        // Almacena el producto más barato en productosPorNombre
+        productosPorNombre[productoMasBarato.producto] = productoMasBarato;
+    });
+
+    const lista = document.getElementById("menorPrecio");
+    lista.innerHTML = ''; // Limpia la lista antes de agregar elementos
+
+    Object.keys(productosPorNombre).forEach(function (nombreProducto) {
+        const elemento = productosPorNombre[nombreProducto];
+        const opcion = `<li>${elemento.producto} - $${elemento.precio} - ${elemento.comercio}</li>`;
+        lista.innerHTML += opcion;
+    });
+
+}
 document.getElementById("listar").onclick = function(){
     productos.forEach(function(elemento){
     const opcion = ` <li> ${elemento.producto}  - $${elemento.precio} - ${elemento.comercio} </li>` ; 
